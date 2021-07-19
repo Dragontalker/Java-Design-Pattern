de9c80f57cd1b8d04602126f1ebf983f108e0155
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:47:22 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
+++ DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
@@ -68,4 +68,17 @@ class D implements Interface1 {
     }
 }
 
+class A {// A类 通过接口Interface1 依赖(使用) B类, 但是只会用到1, 2, 3方法
 
+    public void depend1(Interface1 i) {
+        i.operation1();
+    }
+
+    public void depend2(Interface1 i) {
+        i.operation2();
+    }
+
+    public void depend3(Interface1 i) {
+        i.operation3();
+    }
+}
