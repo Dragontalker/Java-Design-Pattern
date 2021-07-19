47c7e407b2fb071af3d7976276084188f2713d65
Richard <richard.yang.tong@gmail.com>
2021-07-19 14:56:38 -0400

    Added notes.

---
+++ DesignPattern/src/com/dragontalker/uml/Person2.java
@@ -0,0 +1,14 @@
+package com.dragontalker.uml;
+
+public class Person2 {
+    private int id;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
