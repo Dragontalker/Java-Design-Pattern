3d1948a0c6866a7ee42abfedb1f81011ea1dd483
Richard <richard.yang.tong@gmail.com>
2021-07-19 09:34:19 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/liskov/Liskov.java
+++ DesignPattern/src/com/dragontalker/principle/liskov/Liskov.java
@@ -7,6 +7,13 @@ public class Liskov {
         A a = new A();
         System.out.println("11-3=" + a.func1(11, 3));
         System.out.println("1-8=" + a.func1(1, 8));
+
+        System.out.println("------------------------");
+
+        B b = new B();
+        System.out.println("11-3=" + b.func1(11, 3));
+        System.out.println("1-8=" + b.func1(1, 8));
+        System.out.println("11+3+9=" + b.func2(11, 3));
     }
 }
 
