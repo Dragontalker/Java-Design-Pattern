e3a4c4e3af660655dd8f75d81d3a3348710650ae
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:44:07 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
+++ DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
@@ -16,26 +16,26 @@ class B implements Interface1 {
 
     @Override
     public void operation1() {
-
+        System.out.println("B 实现了 operation1....");
     }
 
     @Override
     public void operation2() {
-
+        System.out.println("B 实现了 operation2....");
     }
 
     @Override
     public void operation3() {
-
+        System.out.println("B 实现了 operation3....");
     }
 
     @Override
     public void operation4() {
-
+        System.out.println("B 实现了 operation4....");
     }
 
     @Override
     public void operation5() {
-
+        System.out.println("B 实现了 operation5....");
     }
 }
