c97fc5a60fd65804d214f968f0704f0e86c7cb5a
Richard <richard.yang.tong@gmail.com>
2021-07-19 08:21:05 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/inversion/DependencyInversion.java
+++ DesignPattern/src/com/dragontalker/principle/inversion/DependencyInversion.java
@@ -19,6 +19,8 @@ class Email {
 // 方式1分析
 // 1. 简单, 比较容易想到
 // 2. 如果我们获取的对象是微信, 短信等等, 则需要新增类, 同时Person也要增加相应的接收方法
+// 3. 解决思路: 引入一个抽象的接口: IReceiver, 表示接收者, 这样Person类与接口发生依赖
+//    因为Email, WeiXin 等等属于接收的范围, 他们各自实现IReceiver接口就ok, 这样我们就符合依赖倒转原则
 class Person {
     public void receive(Email email) {
         System.out.println(email.getInfo());
