3fd01ce2d5c790b5cb70881aa774573bcd52faaa
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:12:54 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility3.java
+++ DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility3.java
@@ -11,5 +11,13 @@ class Vehicle2 {
     public void run(String vehicle) {
         System.out.println(vehicle + "在公路上运行....");
     }
+
+    public void runAir(String vehicle) {
+        System.out.println(vehicle + "在天空上运行....");
+    }
+
+    public void runWater(String vehicle) {
+        System.out.println(vehicle + "在水中上运行....");
+    }
 }
 
