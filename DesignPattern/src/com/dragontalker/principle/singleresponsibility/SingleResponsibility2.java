cc82dfe1b636d398a4b96dc57b94090faec73092
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:08:04 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility2.java
+++ DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility2.java
@@ -2,3 +2,9 @@ package com.dragontalker.principle.singleresponsibility;
 
 public class SingleResponsibility2 {
 }
+
+class RoadVehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "在公路上运行....");
+    }
+}
