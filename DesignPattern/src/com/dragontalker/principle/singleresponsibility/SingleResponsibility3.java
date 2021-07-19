e7c8134ce9b94fc2499d69672d9b5ea45be4a3ee
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:23:40 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility3.java
+++ DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility3.java
@@ -5,8 +5,8 @@ public class SingleResponsibility3 {
     public static void main(String[] args) {
 
         Vehicle2 vehicle2 = new Vehicle2();
-        vehicle2.run("摩托车");
-        vehicle2.run("汽车");
+        vehicle2.run("摩托车", 3);
+        vehicle2.run("汽车", 20);
         vehicle2.runWater("轮船");
         vehicle2.runAir("飞机");
     }
@@ -16,8 +16,10 @@ public class SingleResponsibility3 {
 // 1. 这种修改方法没有对原来的类做大的修改, 只是增加方法
 // 2. 这里虽然没有在类这个级别上遵守单一职责原则, 但是在方法级别上, 仍然是遵守单一职责
 class Vehicle2 {
-    public void run(String vehicle) {
-        System.out.println(vehicle + "在公路上运行....");
+    public void run(String vehicle, int time) {
+        if (time > 10 && time < 15) {
+            System.out.println(vehicle + "在公路上运行....");
+        }
     }
 
     public void runAir(String vehicle) {
