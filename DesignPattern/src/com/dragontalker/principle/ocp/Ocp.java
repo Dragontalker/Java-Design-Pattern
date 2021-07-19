621fb612942ddd6fbe3f07133848bcd434ab7ed0
Richard <richard.yang.tong@gmail.com>
2021-07-19 10:44:07 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/ocp/Ocp.java
+++ DesignPattern/src/com/dragontalker/principle/ocp/Ocp.java
@@ -6,3 +6,25 @@ public class Ocp {
 
     }
 }
+
+class GraphicEditor {
+    public void drawShape(Shape s) {
+        if (s.m_type == 1) {
+            drawRectangle(s);
+        } else if (s.m_type == 2) {
+            drawCircle(s);
+        }
+    }
+
+    public void drawRectangle(Shape r) {
+        System.out.println("矩形");
+    }
+
+    public void drawCircle(Shape r) {
+        System.out.println("圆形");
+    }
+}
+
+class Shape {
+    int m_type;
+}
