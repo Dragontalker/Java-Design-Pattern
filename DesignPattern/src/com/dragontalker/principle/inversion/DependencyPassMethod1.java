6ce3342b102c9c206066a5d9765d277a57568fa1
Richard <richard.yang.tong@gmail.com>
2021-07-19 08:33:34 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/inversion/DependencyPassMethod1.java
+++ DesignPattern/src/com/dragontalker/principle/inversion/DependencyPassMethod1.java
@@ -10,7 +10,7 @@ public class DependencyPassMethod1 {
 // 方式1: 通过接口传递实现依赖
 // 开关的接口
 interface IOpenAndClose {
-    void open(ITV tv);
+    void open(ITV tv); // 抽象方法, 接收接口
 }
 
 interface ITV {
@@ -21,5 +21,11 @@ interface ITV {
 class OpenAndClose implements IOpenAndClose {
     public void open(ITV tv) {
         tv.play();
+    }
+}
+
+class SmartTV implements ITV {
+    public void play() {
+        System.out.println("智能电视: 播放中...");
     }
 }
