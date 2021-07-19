d111b252410c577fa0f2ec078cbe5063f2bbe314
Richard <richard.yang.tong@gmail.com>
2021-07-19 19:44:13 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/singletton/type1/SingletonTest01.java
+++ DesignPattern/src/com/dragontalker/singletton/type1/SingletonTest01.java
@@ -2,3 +2,13 @@ package com.dragontalker.singletton.type1;
 
 public class SingletonTest01 {
 }
+
+// 饿汉式(静态变量)
+
+class Singleton {
+
+    // 1. 构造器私有化, 外部不能new
+    private Singleton() {
+
+    }
+}
