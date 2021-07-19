5b04392ba9b8ef5dcd460d26389f684bff4ee808
Richard <richard.yang.tong@gmail.com>
2021-07-18 23:01:18 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/segregation/improve/Segregation1.java
+++ DesignPattern/src/com/dragontalker/principle/segregation/improve/Segregation1.java
@@ -9,6 +9,11 @@ public class Segregation1 {
         a.depend1(new B()); // A类通过接口去依赖B类
         a.depend2(new B());
         a.depend3(new B());
+
+        C c = new C();
+        c.depend1(new D()); // C类通过接口去依赖D类
+        c.depend4(new D());
+        c.depend5(new D());
     }
 }
 
