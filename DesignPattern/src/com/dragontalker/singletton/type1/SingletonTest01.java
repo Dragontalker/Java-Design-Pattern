8bd12f1fc72615a5420b126f12dc55cd578cb8f2
Richard <richard.yang.tong@gmail.com>
2021-07-19 19:46:39 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/singletton/type1/SingletonTest01.java
+++ DesignPattern/src/com/dragontalker/singletton/type1/SingletonTest01.java
@@ -1,6 +1,14 @@
 package com.dragontalker.singletton.type1;
 
 public class SingletonTest01 {
+
+    public static void main(String[] args) {
+
+        // 测试
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);
+    }
 }
 
 // 饿汉式(静态变量)
