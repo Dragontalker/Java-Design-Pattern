f6522fc51f18c6cd442a04d6c2a407139f657bca
Richard <richard.yang.tong@gmail.com>
2021-07-19 19:45:38 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/singletton/type1/SingletonTest01.java
+++ DesignPattern/src/com/dragontalker/singletton/type1/SingletonTest01.java
@@ -15,5 +15,8 @@ class Singleton {
     // 2. 本类内部创建对象实例
     private final static Singleton instance = new Singleton();
 
-
+    // 3. 提供一个共有的静态方法, 返回实例对象
+    public static Singleton getInstance() {
+        return instance;
+    }
 }
