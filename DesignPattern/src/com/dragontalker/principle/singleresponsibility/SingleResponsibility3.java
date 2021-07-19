b0c1b66156f19ca48a50e1bb1c3951d63f8bf531
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:13:56 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility3.java
+++ DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility3.java
@@ -4,6 +4,11 @@ public class SingleResponsibility3 {
 
     public static void main(String[] args) {
 
+        Vehicle2 vehicle2 = new Vehicle2();
+        vehicle2.run("摩托车");
+        vehicle2.run("汽车");
+        vehicle2.runWater("轮船");
+        vehicle2.runAir("飞机");
     }
 }
 
