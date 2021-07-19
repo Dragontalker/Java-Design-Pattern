49b34625f0d5078989071f7f0a504f9bd3055baf
Richard <richard.yang.tong@gmail.com>
2021-07-19 08:15:53 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/inversion/DependencyInversion.java
+++ DesignPattern/src/com/dragontalker/principle/inversion/DependencyInversion.java
@@ -6,3 +6,17 @@ public class DependencyInversion {
 
     }
 }
+
+class Email {
+    public String getInfo() {
+        return "电子邮件信息: Hello, world!";
+    }
+}
+
+// 完成Person接收消息的功能
+// 方式1完成
+class Person {
+    public void receive(Email email) {
+
+    }
+}
