3b3ced382f48cef2ce480484adda4f9501ba6f43
Richard <richard.yang.tong@gmail.com>
2021-07-19 16:09:13 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/uml/aggregation/Computer.java
+++ DesignPattern/src/com/dragontalker/uml/aggregation/Computer.java
@@ -1,8 +1,8 @@
 package com.dragontalker.uml.aggregation;
 
 public class Computer {
-    private Mouse mouse;
-    private Monitor monitor;
+    private Mouse mouse; // 鼠标可以和computer分离
+    private Monitor monitor; // 显示器可以和computer分离
 
     public void setMouse(Mouse mouse) {
         this.mouse = mouse;
