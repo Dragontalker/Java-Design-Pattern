c70e2394b72c672fefbbcc7beb9da450f0d232d9
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:07:02 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility1.java
+++ DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility1.java
@@ -13,6 +13,7 @@ public class SingleResponsibility1 {
 // 交通工具类
 // 方式1
 // 1. 在方式1的方法中, 违反了单一职责原则
+// 2. 解决的方案非常的简单, 根据交通工具运行方式不同, 分解成不同的类即可
 class Vehicle {
     public void run(String vehicle) {
         System.out.println(vehicle + "在公路上运行....");
