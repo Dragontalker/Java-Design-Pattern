53143b6316a42f51b66abcba0cdc4a8441bcf80a
Richard <richard.yang.tong@gmail.com>
2021-07-19 10:46:15 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/ocp/Ocp.java
+++ DesignPattern/src/com/dragontalker/principle/ocp/Ocp.java
@@ -31,3 +31,15 @@ class GraphicEditor {
 class Shape {
     int m_type;
 }
+
+class Rectangle extends Shape {
+    Rectangle() {
+        super.m_type = 1;
+    }
+}
+
+class Circle extends Shape {
+    Circle() {
+        super.m_type = 2;
+    }
+}
