dc90248fee49ff891e5e7f6dce2845b9793cb761
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:42:58 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
+++ DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
@@ -11,3 +11,31 @@ interface Interface1 {
     void operation4();
     void operation5();
 }
+
+class B implements Interface1 {
+
+    @Override
+    public void operation1() {
+
+    }
+
+    @Override
+    public void operation2() {
+
+    }
+
+    @Override
+    public void operation3() {
+
+    }
+
+    @Override
+    public void operation4() {
+
+    }
+
+    @Override
+    public void operation5() {
+
+    }
+}
