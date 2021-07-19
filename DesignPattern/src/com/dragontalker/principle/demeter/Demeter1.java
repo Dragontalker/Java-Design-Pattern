5ed65bc742805e6147a39d5ca1ace49d1c120c0b
Richard <richard.yang.tong@gmail.com>
2021-07-19 11:28:37 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/demeter/Demeter1.java
+++ DesignPattern/src/com/dragontalker/principle/demeter/Demeter1.java
@@ -1,5 +1,6 @@
 package com.dragontalker.principle.demeter;
 
+// 客户端
 public class Demeter1 {
 
     public static void main(String[] args) {
@@ -7,7 +8,21 @@ public class Demeter1 {
     }
 }
 
+// 学校总部员工
 class Employee {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
+// 学院的员工
+class CollegeEmployee {
     private String id;
 
     public String getId() {
