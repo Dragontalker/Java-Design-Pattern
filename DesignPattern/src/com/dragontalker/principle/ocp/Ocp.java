353478761de1384268f2c4d0d20c8b1aaec99587
Richard <richard.yang.tong@gmail.com>
2021-07-19 10:45:05 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/ocp/Ocp.java
+++ DesignPattern/src/com/dragontalker/principle/ocp/Ocp.java
@@ -7,7 +7,9 @@ public class Ocp {
     }
 }
 
+// 这是一个用于绘图的类
 class GraphicEditor {
+    // 接收Shape对象, 然后根据type, 来绘制不同的图形
     public void drawShape(Shape s) {
         if (s.m_type == 1) {
             drawRectangle(s);
@@ -25,6 +27,7 @@ class GraphicEditor {
     }
 }
 
+// Shape类, 基类
 class Shape {
     int m_type;
 }
