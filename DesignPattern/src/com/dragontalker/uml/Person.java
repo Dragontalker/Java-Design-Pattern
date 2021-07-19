2a2fb7ec14c55cfb26468566c4133a148de08f14
Richard <richard.yang.tong@gmail.com>
2021-07-19 14:55:01 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/uml/Person.java
+++ DesignPattern/src/com/dragontalker/uml/Person.java
@@ -4,14 +4,6 @@ public class Person {
     private Integer id;
     private String name;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
