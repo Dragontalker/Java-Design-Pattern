75dd3c815a0db664fc55f60953e8d1fb94255c13
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:42:23 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
+++ DesignPattern/src/com/dragontalker/principle/segregation/Segregation1.java
@@ -2,3 +2,12 @@ package com.dragontalker.principle.segregation;
 
 public class Segregation1 {
 }
+
+// 接口
+interface Interface1 {
+    void operation1();
+    void operation2();
+    void operation3();
+    void operation4();
+    void operation5();
+}
