cb75d6ebcfbaa83d59b690999412406f2b3fcb75
Richard <richard.yang.tong@gmail.com>
2021-07-19 09:38:45 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/liskov/improve/Liskov.java
+++ DesignPattern/src/com/dragontalker/principle/liskov/improve/Liskov.java
@@ -17,8 +17,13 @@ public class Liskov {
     }
 }
 
+// 创建一个更加基础的基类
+class Base {
+    // 把更加基础的方法和成员写到Base类中
+}
+
 // A类
-class A {
+class A extends Base{
     // 返回两个数的查
     public int func1(int num1, int num2) {
         return num1 - num2;
@@ -27,9 +32,8 @@ class A {
 
 // B类继承了A
 // 增加了一个新功能: 完成两个数相加, 然后和9求和
-class B extends A {
+class B extends Base {
     // 这里, 重写了A类的方法, 可能是无意识
-    @Override
     public int func1(int a, int b) {
         return a + b;
     }
