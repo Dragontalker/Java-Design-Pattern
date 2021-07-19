1888bee9cae71f8b4fbb99c3bda884af1190718c
Richard <richard.yang.tong@gmail.com>
2021-07-18 22:11:20 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility2.java
+++ DesignPattern/src/com/dragontalker/principle/singleresponsibility/SingleResponsibility2.java
@@ -13,6 +13,10 @@ public class SingleResponsibility2 {
     }
 }
 
+// 方案2的分析
+// 1. 遵守了单一职责原则
+// 2. 但是这样做的改动很大, 即将类分解, 同时修改客户端
+// 3. 改进: 直接修改Vehicle类, 改动的代码会比较少 => 方案3
 class RoadVehicle {
     public void run(String vehicle) {
         System.out.println(vehicle + "在公路上运行....");
