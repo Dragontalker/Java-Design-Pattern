e248bce058b641c86e5a2fcf6d7d07c01e7a64c1
Richard <richard.yang.tong@gmail.com>
2021-07-19 15:02:55 -0400

    Added notes.

--- DesignPattern/src/com/dragontalker/uml/dependence/PersonServiceBean.java
+++ DesignPattern/src/com/dragontalker/uml/dependence/PersonServiceBean.java
@@ -1,4 +1,15 @@
 package com.dragontalker.uml.dependence;
 
 public class PersonServiceBean {
+    private PersonDao personDao;
+
+    public void save(Person person) {}
+
+    public IDCard getIDCard(Integer personId) {
+        return null;
+    }
+
+    public void modify() {
+        Department department = new Department();
+    }
 }
